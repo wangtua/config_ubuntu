2d0a3c8c373ebc3f292764a97fb1e44fae10a618
wangtua1 <1961498593@qq.com>
2019-09-17 03:21:06 -0400

    update vim ,pwn ,git etc

--- login.sh
+++ login.sh
@@ -18,7 +18,18 @@ mv `which pip` ${pippath}2
 
 ln -s `which pip3` $pippath
 
+pip install ipython
+
 echo "Done! \n"
+
+##################################################################
+
+echo "Update & Config vim"
+
+echo "set mouse-=a" >> ~/.vimrc
+
+echo "Done!"
+
 ##################################################################
 
 echo "Install thefuck "
@@ -30,12 +41,44 @@ echo fuck|fuck
 source ~/.bashrc
 
 echo "Done \n"
+##################################################################
+
+
+
 
 ##################################################################
 
 echo "Update Linux C++ uitls"
 
 echo yes|apt install gdb g++ gcc 
+
+echo "Done!"
+
+##################################################################
+
+echo "Install & Config git"
+
+echo yes|apt install git
+
+#git config --global user.name="XXXXXXXXXX"
+
+#git config --global user.email="XXXXXXXXXX"
+
+echo "Done!"
+
+###################################################################
+
+echo "Install pwntools & pwngdb & peda"
+
+cd ~/
+git clone https://github.com/scwuaptx/Pwngdb.git 
+cp ~/Pwngdb/.gdbinit ~/
+
+git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >> ~/.gdbinit
+echo "DONE! debug your program with gdb and enjoy"
+
+pip3 install git+https://github.com/arthaud/python3-pwntools.git
 
 echo "Done!"
 
