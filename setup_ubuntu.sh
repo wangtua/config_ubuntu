36bba6f771f477239820dbda3c844a632e439da5
wangtua <15527519386@163.com>
2019-10-15 16:17:16 +0800

    add metasploit

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -157,3 +157,9 @@ echo yes | apt install lrzsz
 echo "Done !"
 
 ##############################################################
+
+echo "Install metasploit"
+
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall &&   chmod 755 msfinstall &&   ./msfinstall
+
+echo "Done !!!"
